0daa5ee112e886acc10aa644a3c81a0e8fd56976
Ryora <ryora@example.com>
2026-07-21 14:34:03 +0700

    Add graceful database init for build without DATABASE_URL

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,2 @@
-import Database from "better-sqlite3";
-import path from "path";
-
-const dbPath = path.join(process.cwd(), "data", "ryora.db");
-const db = new Database(dbPath);
-
-db.pragma("journal_mode = WAL");
-db.pragma("foreign_keys = ON");
-
-export default db;
+export { query, getOne, getAll, insert as dbInsert, update as dbUpdate, remove as dbRemove, generateId } from "./postgres";
+export { initializeDatabase } from "./init";
